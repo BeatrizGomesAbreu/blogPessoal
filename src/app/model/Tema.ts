3ecd39563f623e95f7afe1d2a2237ba1fb3f88d0
BeatrizGomesAbreu <beatriz_gome96@yahoo.com.br>
2021-10-05 12:37:01 -0300

    implementação do get e post de temas

--- src/app/model/Tema.ts
+++ src/app/model/Tema.ts
@@ -2,7 +2,7 @@ import { Postagem } from "./Postagem"
 
 export class Tema{
     public id: number
-    public descricacao: string
+    public descricao: string
     public postagem: Postagem[]
     
 }
