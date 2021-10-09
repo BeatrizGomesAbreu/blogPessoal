c99e8029d6a56bd1f9c59071467b8ed0b2b053f5
BeatrizGomesAbreu <beatriz_gome96@yahoo.com.br>
2021-10-09 19:56:33 -0300

    Implementação de alertas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,10 +14,12 @@ import { InicioComponent } from './inicio/inicio.component';
 import { TemaComponent } from './tema/tema.component';
 import { TemaEditComponent } from './edit/tema-edit/tema-edit.component';
 import { TemaDeleteComponent } from './delete/tema-delete/tema-delete.component';
-import { PostagemComponent } from './delete/postagem/postagem.component';
+
 import { PostagemDeleteComponent } from './delete/postagem-delete/postagem-delete.component';
 import { PostagemEditComponent } from './edit/postagem-edit/postagem-edit.component';
 import { UserEditComponent } from './edit/user-edit/user-edit.component';
+import { AlertasComponent } from './alertas/alertas.component';
+import { ModalModule } from 'ngx-bootstrap/modal';
 
 
 
@@ -32,16 +34,17 @@ import { UserEditComponent } from './edit/user-edit/user-edit.component';
     TemaComponent,
     TemaEditComponent,
     TemaDeleteComponent,
-    PostagemComponent,
     PostagemDeleteComponent,
     PostagemEditComponent,
-    UserEditComponent
+    UserEditComponent,
+    AlertasComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    ModalModule.forRoot()
   ],
   providers: [{
     provide: LocationStrategy,
